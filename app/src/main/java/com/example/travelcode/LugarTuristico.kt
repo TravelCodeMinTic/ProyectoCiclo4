70657e5561413f03a3fec3c30fd4b9dddf9eeefe
nataly0914 <nataly0914@hotmail.com>
2021-11-19 22:08:49 -0500

    Cambio de imágenes y contenido sobre lugares turísticos de Colombia

--- app/src/main/java/com/example/travelcode/LugarTuristico.kt
+++ app/src/main/java/com/example/travelcode/LugarTuristico.kt
@@ -2,5 +2,13 @@ package com.example.travelcode
 
 import java.io.Serializable
 
-class LugarTuristico(var titulo:String, var descripcion:String, var imagen:Int, var puntuacion: Double):Serializable  {
+class LugarTuristico(
+    var titulo:String,
+    var descripcion:String,
+    var imagen:Int,
+    var puntuacion: Double,
+    var temperatura: String
+    //var ubicacion: String,
+    //var sitios: String
+    ):Serializable  {
 }
--- app/src/main/java/com/example/travelcode/MainActivity.kt
+++ app/src/main/java/com/example/travelcode/MainActivity.kt
@@ -17,11 +17,11 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         val lugares = ArrayList<LugarTuristico>()
 
-        lugares.add(LugarTuristico("Guatapé, Antioquia", "Uno de los pueblos más coloridos de Colombia, que se refleja en forma de zócalos y calles llenas de vida.", R.drawable.imagen1, 4.0))
-        lugares.add(LugarTuristico("Lugar Turistico #2", "Descripcion de mi lugar turistico el cual se va a investigar...", R.drawable.imagen2, 3.5))
-        lugares.add(LugarTuristico("Lugar Turistico #3", "Descripcion de mi lugar turistico el cual se va a investigar...", R.drawable.imagen3, 2.0))
-        lugares.add(LugarTuristico("Lugar Turistico #4", "Descripcion de mi lugar turistico el cual se va a investigar...", R.drawable.imagen4, 5.0))
-        lugares.add(LugarTuristico("Lugar Turistico #5", "Descripcion de mi lugar turistico el cual se va a investigar...", R.drawable.imagen5, 4.5))
+        lugares.add(LugarTuristico("Guatapé", "Uno de los pueblos más coloridos de Colombia, que se refleja en forma de zócalos y calles llenas de vida.", R.drawable.imagen1, 4.0, temperatura = "19°C"))
+        lugares.add(LugarTuristico("Santa Marta", "La ciudad más antigua de América continental; en una montaña de casi 19.000 pies de altura, cuyas bases se sumergen en un mar tropical.", R.drawable.imagen2, 3.5, temperatura = "32°C"))
+        lugares.add(LugarTuristico("Bogotá, D.C.", "Capital de la República de Colombia y el principal destino turístico del país. Epicentro de diversas actividades y eventos de nivel internacional.", R.drawable.imagen3, 2.0, temperatura = "13°C"))
+        lugares.add(LugarTuristico("Santiago de Cali", "La Sultana del Valle o Sucursal del Cielo, es una ciudad con grandes espacios de valor histórico y de recreación y diversión nocturna y diurna.", R.drawable.imagen4_1, 5.0, temperatura = "28°C"))
+        lugares.add(LugarTuristico("Cartagena de Indias", "La perla colonial de Colombia o La heroica, es una ciudad soñada, una de las más bellas y mejor conservadas de América.", R.drawable.imagen5_1, 4.5, temperatura = "32°C"))
 
         val adapter = CustomAdapter(lugares)
         recyclerView.adapter = adapter
