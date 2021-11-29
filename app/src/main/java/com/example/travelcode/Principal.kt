d86087a0a94d0987b3802fe7b6a9afab256c7038
unknown <diegobauto00@gmail.com>
2021-11-28 22:37:08 -0500

    notificaciones

--- app/src/main/java/com/example/travelcode/Principal.kt
+++ app/src/main/java/com/example/travelcode/Principal.kt
@@ -1,11 +1,18 @@
 package com.example.travelcode
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -92,5 +99,55 @@ class Principal : AppCompatActivity() {
         val notificacionesActivadas : Boolean = preferencias.getBoolean("sync", false)
         val sonidoNotificaciones : Boolean = preferencias.getBoolean("attachment", false)
         Toast.makeText(this, "Checkeado: "+notificacionesActivadas + "y" + sonidoNotificaciones, Toast.LENGTH_SHORT).show()
+        if(notificacionesActivadas){
+            crearNotificacionChannel()
+            crearNotificacion()
+        }
+        else if(notificacionesActivadas && sonidoNotificaciones){
+            crearNotificacionChannel()
+            crearNotificacionConSonido()
+        }
+
     }
+
+    private fun crearNotificacionChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "NOTIFICACION",
+                "TravelCode",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun crearNotificacion(){
+        val builder = NotificationCompat.Builder(this, "NOTIFICACION")
+        builder.setSmallIcon(R.drawable.icono_notificaciones)
+        builder.setContentTitle("Notificacion Android")
+        builder.setContentText("Usted ha recibido una notificación")
+        builder.color = Color.BLUE
+        builder.setTicker("Notificación Nueva")
+        builder.priority = NotificationCompat.PRIORITY_HIGH
+
+        val compat = NotificationManagerCompat.from(this)
+        compat.notify(0, builder.build())
+    }
+
+    private fun crearNotificacionConSonido(){
+        val builder = NotificationCompat.Builder(this, "NOTIFICACION")
+        builder.setSmallIcon(R.drawable.icono_notificaciones)
+        builder.setContentTitle("Notificacion Android")
+        builder.setContentText("Usted ha recibido una notificación")
+        builder.color = Color.BLUE
+        builder.setTicker("Notificación Nueva")
+        builder.priority = NotificationCompat.PRIORITY_HIGH
+
+        builder.setDefaults(Notification.DEFAULT_SOUND)
+
+        val compat = NotificationManagerCompat.from(this)
+        compat.notify(0, builder.build())
+    }
+
 }
